e9b3d1c0cc41f6ccece2d189c68e3ece416c34ca
iamjayy <jason_doan94@yahoo.com>
2021-01-11 17:51:54 -0500

    home page, header, nav, products, routes

---
+++ amazone-clone/src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        alt="header__logo"
+        className="header__logo"
+        src="https://lh3.googleusercontent.com/proxy/WaoSUamyhk4loWz2fH6ZuIdIpALKmCS8aaer0gcSrFFQL2dqIha7x3oUJGKgMQGELK6GFapowQnrapEGYN76zpOu5pJKytp2FprQLThcf0rRpotjt_-JA-0UsLB7YM-kWlwpWZAmdMw"
+      />
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign in</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="Header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ amazone-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDg2BBXv1NYv4XOrJNjbUHQqZx33JVzOpw",
+  authDomain: "challenge-bbb92.firebaseapp.com",
+  projectId: "challenge-bbb92",
+  storageBucket: "challenge-bbb92.appspot.com",
+  messagingSenderId: "198488876781",
+  appId: "1:198488876781:web:9468293a4084727d254ee4",
+  measurementId: "G-D1BYZL68NQ",
+};

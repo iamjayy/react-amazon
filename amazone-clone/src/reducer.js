16b8f837eec9c817c67ba35c24c8567dc87d401e
iamjayy <jason_doan94@yahoo.com>
2021-01-12 01:09:04 -0500

    adding total price in subtotal.js

--- amazone-clone/src/reducer.js
+++ amazone-clone/src/reducer.js
@@ -2,6 +2,13 @@ export const initialState = {
   basket: [],
 };
 
+//selector
+//cart total price
+//initial amount, iterate through each item
+//every time it loops though i want the item price to add to total amount and the initial amount is zero and return as value
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
@@ -10,7 +17,6 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
-
     default:
       return state;
   }

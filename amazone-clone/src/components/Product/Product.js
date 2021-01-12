b484f8a28dbb34c723697bbe2f6ebeecec321fc1
iamjayy <jason_doan94@yahoo.com>
2021-01-12 02:09:05 -0500

    checkout product dynamically add and delete from cart

--- amazone-clone/src/components/Product/Product.js
+++ amazone-clone/src/components/Product/Product.js
@@ -5,15 +5,14 @@ import { useStateValue } from "../../StateProvider";
 function Product({ id, title, image, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
 
-  console.log("this is the basket", basket);
-
   const addToBasket = () => {
-    //dispatch item into the data layer
+    // dispatch the item into the data layer
     dispatch({
       type: "ADD_TO_BASKET",
       item: {
         id: id,
         title: title,
+        image: image,
         price: price,
         rating: rating,
       },

eabfe4eb8043eff783822e426ce50dcc5b137a41
iamjayy <jason_doan94@yahoo.com>
2021-01-21 21:15:06 -0500

    user auth finish

--- amazone-clone/src/App.js
+++ amazone-clone/src/App.js
@@ -1,11 +1,36 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout/Checkout";
 import Login from "./components/Login/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //will only run once when the app component loads
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is > ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+        //user is logged out
+      }
+    });
+  }, []);
   return (
     //BEM
     <Router>
